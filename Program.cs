f0a67743d8e38da1b2376fdce6d313579c4f33c1
Sonnnyyy04 <sanseinikolai83@gmail.com>
2024-10-22 17:35:02 +1000

    Auth first commit dev branch

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Numerics;
-
-class Program
+﻿class Program
 {
     static string GenerateNumber()
     {
@@ -11,7 +8,8 @@ class Program
         while (number.Length < 4)
         {
             int digit = rand.Next(0, 10);
-            if (!number.Contains(digit.ToString())){
+            if (!number.Contains(digit.ToString()))
+            {
                 number += digit.ToString();
             }
         }
@@ -42,15 +40,44 @@ class Program
 
         return (correctNumbers, correctPositions);
     }
+
+    static bool AuthenticateUser()
+    {
+        string filePath = "user.txt";
+        Console.Write("Введите логин: ");
+        string inputLogin = Console.ReadLine();
+        Console.Write("Введите пароль: ");
+        string inputPassword = Console.ReadLine();
+
+        // Чтение файла с пользователями
+        if (System.IO.File.Exists(filePath))
+        {
+            string[] users = System.IO.File.ReadAllLines(filePath);
+            foreach (string user in users)
+            {
+                var credentials = user.Split(' ');
+                if (credentials[0] == inputLogin && credentials[1] == inputPassword)
+                {
+                    LogAction($"Пользователь {inputLogin} успешно авторизовался.");
+                    return true;
+                }
+            }
+        }
+
+        Console.WriteLine("Неверный логин или пароль. Попробуйте снова.");
+        LogAction($"Неудачная попытка входа для логина {inputLogin}.");
+        return false;
+    }
+
     static void UpdateRanking(int attempts)
     {
         string filePath = "ranking.txt";
 
         // Чтение существующего рейтинга
         List<int> rankings = new List<int>();
-        if (File.Exists(filePath))
+        if (System.IO.File.Exists(filePath))
         {
-            string[] lines = File.ReadAllLines(filePath);
+            string[] lines = System.IO.File.ReadAllLines(filePath);
             int prevAttempts;
             foreach (var line in lines)
             {
@@ -67,15 +94,25 @@ class Program
         rankings.Sort();
 
         // Запись обновлённого рейтинга в файл
-        File.WriteAllLines(filePath, rankings.ConvertAll(x => x.ToString()));
+        System.IO.File.WriteAllLines(filePath, rankings.ConvertAll(x => x.ToString()));
 
         int position = rankings.IndexOf(attempts) + 1;
         Console.WriteLine($"Ты занимаешь {position}-е место в рейтинге с {attempts} попытками.");
+        LogAction($"Игрок завершил игру за {attempts} попыток и занял {position}-е место в рейтинге.");
     }
 
+    static void LogAction(string message)
+    {
+        Console.WriteLine($"{DateTime.Now}: {message}");
+    }
 
     static void Main(string[] args)
     {
+        Console.WriteLine("Добро пожаловать в игру!");
+        
+        while (!AuthenticateUser()) {}
+
+        // Начало игры после успешной авторизации
         string secretNumber = GenerateNumber();
         Console.WriteLine("Игра началась! Угадай 4-значное число без повторяющихся цифр.");
 
